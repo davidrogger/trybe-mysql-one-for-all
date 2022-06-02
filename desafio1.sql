e05e6a7fa94a75d4ae6599afaa795ad185508930
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-02 19:49:26 -0300

    feat:Desafio01
    inserindo dados na tabela de usuários

--- desafio1.sql
+++ desafio1.sql
@@ -20,3 +20,16 @@ CREATE TABLE SpotifyClone.user (
   plan_id INT,
   FOREIGN KEY (plan_id) REFERENCES plan(id)
 );
+
+INSERT SpotifyClone.user(`name`, age, signure_date, plan_id) VALUES
+  ('Thati', 23, '2019-10-20', 1),
+  ('Cintia', 35, '2017-12-30', 4),
+  ('Bill', 20, '2019-06-05', 2),
+  ('Roger', 45, '2020-05-13', 3),
+  ('Norman', 58, '2017-02-17', 3),
+  ('Patrick', 33, '2017-01-06', 4),
+  ('Vivian', 26, '2018-01-05', 2),
+  ('Carol', 19, '2018-02-14', 2),
+  ('Angelina', 42, '2018-04-29', 4),
+  ('Paul', 46, '2017-01-17', 4);
+
