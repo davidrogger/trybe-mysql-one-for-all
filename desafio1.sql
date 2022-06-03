85ce483b60e6b4c5c922de65e7c408f09f26eb7c
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-03 10:05:54 -0300

    feat:Desafio01
    inserindo dados na tabela de lista de seguidores

--- desafio1.sql
+++ desafio1.sql
@@ -177,3 +177,28 @@ CREATE TABLE SpotifyClone.follow_list(
   FOREIGN KEY (`user_id`) REFERENCES user(id),
   FOREIGN KEY (artist_id) REFERENCES artist(id)
 );
+
+INSERT SpotifyClone.follow_list(`user_id`, artist_id)
+VALUES
+  (1, 1),
+  (1, 4),
+  (1, 3),
+  (2, 1),
+  (2, 3),
+  (3, 2),
+  (3, 1),
+  (4, 4),
+  (5, 5),
+  (5, 6),
+  (6, 6),
+  (6, 3),
+  (6, 1),
+  (7, 2),
+  (7, 5),
+  (8, 1),
+  (8, 5),
+  (9, 6),
+  (9, 4),
+  (9, 3),
+  (10, 2),
+  (10, 6);
