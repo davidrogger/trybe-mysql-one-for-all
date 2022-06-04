a90978f1d32a8ab71d81ca4890654f535ccd6ac3
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-04 14:58:04 -0300

    fix:desafio1
    indentação do select

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,7 @@
 SELECT
 	ar.`name` AS 'artista',
-    al.`name` AS 'album',
-    COUNT(fl.artist_id) AS 'seguidores'
+  al.`name` AS 'album',
+  COUNT(fl.artist_id) AS 'seguidores'
 FROM SpotifyClone.artist AS ar
 INNER JOIN SpotifyClone.album AS al ON al.artist_id = ar.id
 INNER JOIN SpotifyClone.follow_list AS fl ON fl.artist_id = ar.id
