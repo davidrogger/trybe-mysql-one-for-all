01e0b53bfd19d03b153578a88799bcd9b986dcd0
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-04 14:55:58 -0300

    feat:desafio7
    query mostrando artistas com seus albuns e quantidade de seguidores

--- desafio7.sql
+++ desafio7.sql
@@ -1,10 +1,9 @@
--- SELECT
--- 	artist.`name` AS 'artista',
---     album.`name` AS 'album',
---     COUNT(artist.id) AS 'seguidores'
--- FROM SpotifyClone.artist
--- INNER JOIN SpotifyClone.album ON album.artist_id = artist.id
--- INNER JOIN SpotifyClone.follow_list ON follow_list.artist_id = artist.id;
-
--- SELECT COUNT(*) FROM SpotifyClone.follow_list
--- WHERE artist_id = 8;
+SELECT
+	ar.`name` AS 'artista',
+    al.`name` AS 'album',
+    COUNT(fl.artist_id) AS 'seguidores'
+FROM SpotifyClone.artist AS ar
+INNER JOIN SpotifyClone.album AS al ON al.artist_id = ar.id
+INNER JOIN SpotifyClone.follow_list AS fl ON fl.artist_id = ar.id
+GROUP BY al.id
+ORDER BY seguidores DESC, artista ASC, album ASC;
